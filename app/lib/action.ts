66c23634315fc08457b60bf8d053eaf8a4d5ddb9
Pichaipusit Kamsorn <Pichaipusit.ks@gmail.com>
2024-10-01 20:10:36 +0700

    sol: Improving form accessibility

--- app/lib/action.ts
+++ app/lib/action.ts
@@ -5,15 +5,14 @@ import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
 import { z } from "zod";
 
-// TODO: Make these field show message error
-//  1. customerId -> "Please select a customer."
-//  1. amount -> "Please enter an amount greater than $0."
 const FormSchema = z.object({
   id: z.string(),
 
-  customerId: z.string(),
+  customerId: z.string({ invalid_type_error: "Please select a customer." }),
 
-  amount: z.coerce.number(),
+  amount: z.coerce
+    .number()
+    .gt(0, { message: "Please enter an amount greater than $0." }),
 
   status: z.enum(["pending", "paid"], {
     invalid_type_error: "Please select an invoice status.",
@@ -24,12 +23,30 @@ const FormSchema = z.object({
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
 const UpdateInvoice = FormSchema.omit({ id: true, date: true });
 
+export type State = {
+  errors?: {
+    customerId?: string[];
+    amount?: string[];
+    status?: string[];
+  };
+  message?: string | null;
+};
+
 export async function createInvoice(prevState: State, formData: FormData) {
-  //TODO: Validate form using Zod
+  const validatedFields = CreateInvoice.safeParse({
+    customerId: formData.get("customerId"),
+    amount: formData.get("amount"),
+    status: formData.get("status"),
+  });
 
-  //TODO: If form validation fails, return errors early. Otherwise, continue.
+  if (!validatedFields.success) {
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+      message: "Missing Fields. Failed to Create Invoice.",
+    };
+  }
 
-  //TODO: Prepare data for insertion into the database
+  const { customerId, amount, status } = validatedFields.data;
   const amountInCents = amount * 100;
   const date = new Date().toISOString().split("T")[0];
 
--- app/ui/invoices/create-form.tsx
+++ app/ui/invoices/create-form.tsx
@@ -13,10 +13,11 @@ import { createInvoice, State } from "@/app/lib/action";
 import { useActionState } from "react";
 
 export default function Form({ customers }: { customers: CustomerField[] }) {
-  //TODO: Can u get error data from zod that store in action of Next?
+  const initialState: State = { errors: {}, message: null };
+  const [state, formAction] = useActionState(createInvoice, initialState);
 
   return (
-    <form action={}>
+    <form action={formAction}>
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
         {/* Customer Name */}
         <div className="mb-4">
@@ -29,7 +30,7 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
               name="customerId"
               className="peer block w-full cursor-pointer rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
               defaultValue=""
-              //TODO: Connect this to customer error element
+              aria-describedby="customer-error"
             >
               <option value="" disabled>
                 Select a customer
@@ -43,7 +44,14 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
             <UserCircleIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500" />
           </div>
 
-          {/* TODO: Show customer error from zod validation*/}
+          <div id="customer-error" aria-live="polite" aria-atomic="true">
+            {state.errors?.customerId &&
+              state.errors?.customerId?.map((error: string) => (
+                <p className="mt-2 text-sm text-red-500" key={error}>
+                  {error}
+                </p>
+              ))}
+          </div>
         </div>
 
         {/* Invoice Amount */}
